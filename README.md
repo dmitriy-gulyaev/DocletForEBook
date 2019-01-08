9ca3dbe44e3f1f50e4bc020cd64bea1b048033e4
dmitriy_gulyaev <dmitriy_gulyaev@epam.com>
2019-01-08 20:39:03 +0400

    Printing Constructors list

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DocletForEBook
+
+Doclet to generate JavaDoc HTML-files for my 6,0 " e-book.
--- src/main/java/com/tr4w/DocletForEBook.java
+++ src/main/java/com/tr4w/DocletForEBook.java
@@ -5,12 +5,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-import com.sun.javadoc.ClassDoc;
-import com.sun.javadoc.Doc;
-import com.sun.javadoc.FieldDoc;
-import com.sun.javadoc.MethodDoc;
-import com.sun.javadoc.RootDoc;
-import com.sun.javadoc.Tag;
+import com.sun.javadoc.*;
 
 public class DocletForEBook {
 
@@ -32,6 +27,7 @@ public class DocletForEBook {
             try (java.io.FileWriter fileWriter = new java.io.FileWriter(file)) {
                 fileWriter.write("<html><head>");
                 fileWriter.write("<title>" + classDoc.typeName() + "</title>");
+				//fileWriter.write("<style> u {color: #ffffff; background: #444444; text-decoration-line: none;}</style>");
                 fileWriter.write("</head><body>");
 
                 if (classDoc.isClass()) {
@@ -56,6 +52,7 @@ public class DocletForEBook {
 
                 printTags(fileWriter, classDoc.tags());
 
+				printDoc(fileWriter, "Constructors ", classDoc.constructors());
                 printDoc(fileWriter, "Fields", classDoc.fields());
                 printDoc(fileWriter, "Methods", classDoc.methods());
 
@@ -72,19 +69,33 @@ public class DocletForEBook {
         }
         fileWriter.write("<h4>" + name + " (" + docs.length + ")</h4>");
         fileWriter.write("<ul>");
-        for (int c = 0; c < docs.length; ++c) {
+		
+		Arrays.sort(docs);
+		for (int c = 0; c < docs.length; ++c) {
             Doc doc = docs[c];
             String type = "";
             String signature = "";
-            if (doc instanceof MethodDoc) {
-                type = ((MethodDoc) doc).returnType().toString();
 
-                signature = Arrays.toString(((MethodDoc) doc).parameters());
-            }
-            if (doc instanceof FieldDoc) {
+			if (doc instanceof ConstructorDoc) {
+				ConstructorDoc constructorDoc = (ConstructorDoc) doc;
+				signature = Stream.of(constructorDoc.parameters()).map(DocletForEBook::parameterToString).collect(java.util.stream.Collectors.joining(", "," (",")"));
+            } else if (doc instanceof MethodDoc) {
+				MethodDoc methodDoc = (MethodDoc) doc;
+				ParameterizedType parameterizedType = methodDoc.returnType().asParameterizedType();
+				if (1 == 2 && parameterizedType != null){
+                    type = methodDoc.returnType().asParameterizedType().asClassDoc().toString();
+				} else{
+					type = methodDoc.returnType().simpleTypeName();
+				}
+
+				signature = Stream.of(methodDoc.parameters()).map(DocletForEBook::parameterToString).collect(java.util.stream.Collectors.joining(", "," (",")"));
+            } else if (doc instanceof FieldDoc) {
                 type = ((FieldDoc) doc).type().toString();
             }
             fileWriter.write("<li><b>" + (c + 1) + "</b>. ");
+			if (((com.sun.javadoc.ProgramElementDoc) doc).isStatic()) {
+				fileWriter.write("static ");
+			}
             fileWriter.write(type + " <b>" + doc.name() + "</b>" + signature + "<br/>");
             fileWriter.write(replaceTags(doc.commentText()));
             printTags(fileWriter, doc.tags());
@@ -94,31 +105,38 @@ public class DocletForEBook {
         }
         fileWriter.write("</ul>");
     }
-
+	
+	private static String parameterToString(com.sun.javadoc.Parameter parameter) {
+		return "<b><tt>"+parameter.type() + "</tt></b> " + parameter.name();
+		
+		
+	}
 
     private static void printTags(FileWriter fileWriter, Tag[] tags) throws IOException {
         if (tags.length > 0) {
-            fileWriter.write("<br/>");
+            //fileWriter.write("<br/>");
             Stream.of(tags).filter(DocletForEBook::filterTag).forEach(tag -> {
                 try {
-                    fileWriter.write("<br/><b>" + tag.toString()
+                    fileWriter.write("<br/><b>" + replaceTags(tag.toString())
                             // .replace("<", "&lt;")
                             .replace(":", "</b>: ").substring(1));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
-            fileWriter.write("<br/>");
+            //fileWriter.write("<br/>");
         }
     }
 
     private static boolean filterTag(Tag tag) {
-        return true;// tag.name().equals("@param");
+        return !(tag.name().equals("@param") || tag.name().equals("@return"));
     }
 
 
     private static String replaceTags(String comment) {
-
+		if (comment.indexOf('{') == -1){
+			return comment;
+		}
         // if(1==1)return comment;
 
         StringBuilder tempArray = new StringBuilder(comment.length() + 100);
@@ -162,7 +180,7 @@ public class DocletForEBook {
 
     enum TagType {
 
-        LINK("u"), CODE("tt");
+        LINK("b"), CODE("tt");
 
         TagType(String htmlTag) {
             this.htmlTag = htmlTag;
